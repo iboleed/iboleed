444c805f3b8f03c1789e27563c4d2b0cded34f9d
pvl <activated@github.com>
2019-03-13 16:04:13 +0200

    Init

---
+++ src/main/java/com/iboleed/IboleedApplication.java
@@ -0,0 +1,46 @@
+package com.iboleed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableConfigServer
+@RestController
+@RequestMapping("/books")
+public class IboleedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IboleedApplication.class, args);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceApplication.class, args);
+	}
+
+	private List<Book> bookList = Arrays.asList(
+			new Book(1L, "Baeldung goes to the market", "Tim Schimandle"),
+			new Book(2L, "Baeldung goes to the park", "Slavisa")
+	);
+
+	@GetMapping("")
+	public List<Book> findAllBooks() {
+		return bookList;
+	}
+
+	@GetMapping("/{bookId}")
+	public Book findBook(@PathVariable Long bookId) {
+		return bookList.stream().filter(b -> b.getId().equals(bookId)).findFirst().orElse(null);
+	}
+
+	class Book {
+    private Long id;
+    private String author;
+    private String title;
+ 
+		    // standard getters and setters
+
+	}
+
+}
+
