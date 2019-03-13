8afd92943996e51beb4d1943f46e544c9ce09355
pvl <activated@github.com>
2019-03-13 16:31:39 +0200

    compile fix

--- src/main/java/com/iboleed/IboleedApplication.java
+++ src/main/java/com/iboleed/IboleedApplication.java
@@ -6,41 +6,41 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 //@EnableDiscoveryClient
 //@EnableConfigServer
-@RestController
-@RequestMapping("/books")
+//@RestController
+//@RequestMapping("/books")
 public class IboleedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IboleedApplication.class, args);
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookServiceApplication.class, args);
-	}
-
-	private List<Book> bookList = Arrays.asList(
-			new Book(1L, "Baeldung goes to the market", "Tim Schimandle"),
-			new Book(2L, "Baeldung goes to the park", "Slavisa")
-	);
-
-	@GetMapping("")
-	public List<Book> findAllBooks() {
-		return bookList;
-	}
-
-	@GetMapping("/{bookId}")
-	public Book findBook(@PathVariable Long bookId) {
-		return bookList.stream().filter(b -> b.getId().equals(bookId)).findFirst().orElse(null);
-	}
-
-	class Book {
-    private Long id;
-    private String author;
-    private String title;
- 
-		    // standard getters and setters
-
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(BookServiceApplication.class, args);
+//	}
+//
+//	private List<Book> bookList = Arrays.asList(
+//			new Book(1L, "Baeldung goes to the market", "Tim Schimandle"),
+//			new Book(2L, "Baeldung goes to the park", "Slavisa")
+//	);
+//
+//	@GetMapping("")
+//	public List<Book> findAllBooks() {
+//		return bookList;
+//	}
+//
+//	@GetMapping("/{bookId}")
+//	public Book findBook(@PathVariable Long bookId) {
+//		return bookList.stream().filter(b -> b.getId().equals(bookId)).findFirst().orElse(null);
+//	}
+//
+//	class Book {
+//    private Long id;
+//    private String author;
+//    private String title;
+// 
+//		    // standard getters and setters
+//
+//	}
 
 }
 
--- src/test/java/com/iboleed/IboleedApplicationTests.java
+++ src/test/java/com/iboleed/IboleedApplicationTests.java
@@ -5,5 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IboleedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
 
 }
