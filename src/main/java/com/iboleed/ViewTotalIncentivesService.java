0adfb9bc5d4007ea35afce13254af0719495d42c
Vasyl Khyzhniak <vasyl.khyzhniak@eleks.com>
2019-03-14 16:16:06 +0200

    add totalincentives draft service and contrler

--- src/main/java/com/iboleed/ViewTotalIncentivesService.java
+++ src/main/java/com/iboleed/ViewTotalIncentivesService.java
@@ -1,6 +1,10 @@
 package com.iboleed;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ViewTotalIncentivesService {
+
     public Integer getTotalPoints(Long memberId) {
         return 1;
     }
